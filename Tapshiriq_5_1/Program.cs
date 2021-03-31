57b796222bfdd8c62a0546c06b65163b1a0b47bd
HasanliGunel <hasanligunel@hotmail.com>
2021-03-31 08:32:03 +0400

    first commit

---
+++ Tapshiriq_5_1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Tapshiriq_5_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            if (n > 0 & n <= 100)
+            {
+
+                
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = 1; j <= n; j++)  
+                    {
+                        Console.Write(" ");
+                        if (j <= n - i)
+                            Console.Write("#");
+                        if (j == n)
+                            Console.Write('\n');
+
+                    }
+                    
+                }
+            }
+            else
+                Console.WriteLine("n 0 < n <= 100 araliginda olmalidir.");
+                
+        }
+    }
+}
